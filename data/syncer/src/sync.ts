2e55b3c0a30695b00524c3366b675abe01b5c3c8
Mitchell Grice <mitch@gricey.net>
2020-09-28 23:10:32 +1000

    Data syncer batch s3 uploads. Fix data uq 2020-2 PHYS7073

--- data/syncer/src/sync.ts
+++ data/syncer/src/sync.ts
@@ -3,6 +3,7 @@
  */
 import hasha from "hasha";
 import dir from "node-dir";
+import pLimit from "p-limit";
 import {promises as fs} from "fs";
 import {S3Client} from '@aws-sdk/client-s3-node/S3Client';
 import {ListObjectsV2Command, ListObjectsV2Output} from "@aws-sdk/client-s3-node";
@@ -35,6 +36,8 @@ async function getS3FileHashes(): Promise<Map<string, string>> {
             result.set(file.Key!.slice(s3Prefix.length), file.ETag!.replace(/"/g, ""));  // Etag values
         });
     } while (continuationToken);
+
+    console.log(`${result.size} s3 files`);
     return result;
 }
 
@@ -47,6 +50,8 @@ async function getLocalFileHashes(): Promise<Map<string, string>> {
         const hash = await hasha.fromFile(filePrefix + f, {algorithm: "md5"});
         result.set(f.replace("\\", "/"), hash);
     }
+
+    console.log(`${result.size} local files`);
     return result;
 }
 
@@ -54,23 +59,27 @@ async function run(): Promise<void> {
     console.log("Starting syncer");
     // Get the hashes of each file locally and on S3
     const [s3Map, localMap] = await Promise.all([getS3FileHashes(), getLocalFileHashes()]);
-    console.log(`${localMap.size} local files`);
-    console.log(`${s3Map.size} s3 files`);
 
     // Upload files which don't match or don't exist remotely
-    for (const [key, localHash] of localMap.entries()) {
-        const s3Hash = s3Map.get(key);
-        if (s3Hash == undefined || localHash !== s3Hash) {
-            console.log(`Uploading ${key}`);
-            const body = await fs.readFile(filePrefix + key);
-            await s3.send(new PutObjectCommand({
-                Body: body,
-                Bucket: s3Bucket,
-                ContentType: "application/json",
-                Key: "data/" + key,
-            }));
-        }
-    }
+    const limiter = pLimit(10);
+    const uploadPromises = Array.from(localMap.entries())
+        .filter(([key, localHash]) => {
+            const s3Hash = s3Map.get(key);
+            return s3Hash === undefined || localHash !== s3Hash;
+        })
+        .map(([key, localHash]) => {
+            return limiter(async () => {
+                console.log(`Uploading ${key}`);
+                const body = await fs.readFile(filePrefix + key);
+                await s3.send(new PutObjectCommand({
+                    Body: body,
+                    Bucket: s3Bucket,
+                    ContentType: "application/json",
+                    Key: "data/" + key,
+                }));
+            });
+        });
+    await Promise.all(uploadPromises);
 }
 
 run()
